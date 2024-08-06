b9b365180f3327001be5b97fb920eff1cad190a7
Facundo <140009867+FacuuC@users.noreply.github.com>
2024-08-05 21:04:33 -0300

    Proyecto-semiFinalizado
    Se agrega el proyecto en un estado avanzado con las tecnologías de HTML, CSS y JavaScript

---
+++ app.js
@@ -0,0 +1,73 @@
+    let parrafoResultado = document.getElementById("parrafoResultado");
+    let parrafo = document.getElementById("parrafo");
+    let h2Viejo = document.getElementById("h2Bloque2");
+    let imgBloque2 = document.getElementById("imgBloque2");
+    let i = 0;
+    let contenedorBloque2 =  document.getElementById("contenedor__bloque2")
+    let botonCopiar = document.getElementById("boton__copiar");
+    let input_texto = document.getElementById("caja__texto");
+
+function Encriptar(){
+    let input_texto = document.getElementById("caja__texto").value; 
+    let input_texto_lowerCase = input_texto.toLowerCase();
+    if(input_texto.length != 0){
+        if(input_texto === input_texto_lowerCase){
+            textoCifrado = input_texto
+            .replace(new RegExp("e", "g"),"enter")
+            .replace(new RegExp("i", "g"),"imes")
+            .replace(new RegExp("a", "g"),"ai")
+            .replace(new RegExp("o", "g"),"ober")
+            .replace(new RegExp("u", "g"),"ufat")
+            
+            AsignarTextoElemento(textoCifrado);
+        } else {
+            alert("Debe ingresar texto en mínuscula")
+        }
+    } else {
+        alert("Debes ingresar al menos 1 carácter")
+        parrafo.innerHTML = "Ingresa el texto que desees encriptar o desencriptar";
+        parrafoResultado.innerHTML = "";
+        h2Viejo.innerHTML = "Ningún mensaje fue encontrado";
+        imgBloque2.removeAttribute("hidden");
+        contenedorBloque2.setAttribute("class","contenedor__bloque2");
+        botonCopiar.setAttribute("", "hidden");
+    }
+}
+
+function Desencriptar(){
+    let input_texto = document.getElementById("caja__texto").value;
+    if(input_texto.length != 0){
+        textoDescifrado = input_texto
+        .replace(new RegExp("enter", "g"),"e")
+        .replace(new RegExp("imes", "g"),"i")
+        .replace(new RegExp("ai", "g"),"a")
+        .replace(new RegExp("ober", "g"),"o")
+        .replace(new RegExp("ufat", "g"),"u")
+        
+        AsignarTextoElemento(textoDescifrado);
+
+    } else {
+        alert("Debes ingresar texto para encriptar")
+        parrafo.innerHTML = "Ingresa el texto que desees encriptar o desencriptar";
+        parrafoResultado.innerHTML = "";
+        h2Viejo.innerHTML = "Ningún mensaje fue encontrado";
+        imgBloque2.removeAttribute("hidden");
+        contenedorBloque2.setAttribute("class","contenedor__bloque2");
+        botonCopiar.setAttribute("", "hidden");
+    }
+}
+
+function AsignarTextoElemento(texto) {
+    contenedorBloque2.setAttribute("class","contenedor__bloque2__encriptar");
+    parrafoResultado.innerHTML = texto;
+    h2Viejo.innerHTML = "";
+    imgBloque2.setAttribute("hidden", "");
+    parrafo.innerHTML = "";
+    botonCopiar.removeAttribute("hidden")
+
+}
+
+function copiarAlPortapapeles (){
+    let textoCopiado = document.getElementById("parrafoResultado").innerHTML;
+    navigator.clipboard.writeText(textoCopiado);
+}
