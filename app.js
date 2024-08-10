8d9288f6266b9fe4603b2bac04c4e87c9c56f579
Facundo <140009867+FacuuC@users.noreply.github.com>
2024-08-10 11:09:52 -0300

    actualizado el HTML, CSS, y JS
    Se ha trabajado en la responsividad de la pagina con CSS y con JS. También se ha agregado la instancia para comprobar acentos en el texto de entrada, y se han realizado cambios en la organización de las líneas de código para mayor legibilidad.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
     let h2Viejo = document.getElementById("h2Bloque2");
     let muñeco = document.getElementById("muñeco");
     let i = 0;
+    let acento = "´";
     let contenedorBloque2 =  document.getElementById("contenedor__bloque2")
     let botonCopiar = document.getElementById("boton__copiar");
     let input_texto = document.getElementById("caja__texto");
@@ -18,32 +19,30 @@
     mediaBp.addListener(changeSize);
     changeSize(mediaBp);
 
-
-
 function Encriptar(){
     let input_texto = document.getElementById("caja__texto").value; 
     let input_texto_lowerCase = input_texto.toLowerCase();
     if(input_texto.length != 0){
         if(input_texto === input_texto_lowerCase){
-            textoCifrado = input_texto
-            .replace(new RegExp("e", "g"),"enter")
-            .replace(new RegExp("i", "g"),"imes")
-            .replace(new RegExp("a", "g"),"ai")
-            .replace(new RegExp("o", "g"),"ober")
-            .replace(new RegExp("u", "g"),"ufat")
-            
-            AsignarTextoElemento(textoCifrado);
+            eliminarAcentos(input_texto);
+            if (input_texto === input_textoSinAcentos){
+                textoCifrado = input_texto
+                .replace(new RegExp("e", "g"),"enter")
+                .replace(new RegExp("i", "g"),"imes")
+                .replace(new RegExp("a", "g"),"ai")
+                .replace(new RegExp("o", "g"),"ober")
+                .replace(new RegExp("u", "g"),"ufat")
+                
+                AsignarTextoElemento(textoCifrado);
+            }
+            else{
+                alert("Debe ingresar texto sin acentos");
+            }
         } else {
             alert("Debe ingresar texto en mínuscula")
         }
     } else {
-        alert("Debes ingresar al menos 1 carácter");
-        parrafo.innerHTML = "Ingresa el texto que desees encriptar o desencriptar";
-        parrafoResultado.innerHTML = "";
-        h2Viejo.innerHTML = "Ningún mensaje fue encontrado";
-        muñeco.removeAttribute("hidden");
-        contenedorBloque2.setAttribute("class","contenedor__bloque2");
-        botonCopiar.setAttribute("hidden", "");
+        normalizarPanelEncriptado();
     }
 }
 
@@ -60,28 +59,43 @@ function Desencriptar(){
         AsignarTextoElemento(textoDescifrado);
 
     } else {
-        alert("Debes ingresar al menos 1 carácter")
-        parrafo.innerHTML = "Ingresa el texto que desees encriptar o desencriptar";
-        parrafoResultado.innerHTML = "";
-        h2Viejo.innerHTML = "Ningún mensaje fue encontrado";
-        muñeco.removeAttribute("hidden");
-        contenedorBloque2.setAttribute("class","contenedor__bloque2");
-        botonCopiar.setAttribute("hidden", "");
+        normalizarPanelEncriptado()
     }
 }
 
 function AsignarTextoElemento(texto) {
-    contenedorBloque2.setAttribute("class","contenedor__bloque2__encriptar");
+    contenedorBloque2.setAttribute("class","contenedor__bloque2__encriptado");
     parrafoResultado.innerHTML = texto;
     h2Viejo.innerHTML = "";
     muñeco.setAttribute("hidden", "");
     parrafo.innerHTML = "";
-    botonCopiar.removeAttribute("hidden")
+    botonCopiar.removeAttribute("hidden");
+}
 
+function normalizarPanelEncriptado(){
+    alert("Debes ingresar al menos 1 carácter");
+        parrafo.innerHTML = "Ingresa el texto que desees encriptar o desencriptar";
+        parrafoResultado.innerHTML = "";
+        h2Viejo.innerHTML = "Ningún mensaje fue encontrado";
+        contenedorBloque2.setAttribute("class","contenedor__bloque2");
+        botonCopiar.setAttribute("hidden", "");
+        if (mediaBp.matches === true){
+            muñeco.removeAttribute("hidden");
+        }
 }
 
 function copiarAlPortapapeles (){
     let textoCopiado = document.getElementById("parrafoResultado").innerHTML;
     navigator.clipboard.writeText(textoCopiado);
+    alert("Texto copiado al portapapeles");
 }
 
+function eliminarAcentos(texto){
+    input_textoSinAcentos = texto
+                                .replace(/\u00E1/g,"a")
+                                .replace(/\u00E9/g,"e")
+                                .replace(/\u00ED/g,"i")
+                                .replace(/\u00F3/g,"o")
+                                .replace(/\u00FA/g,"u")
+    return texto;
+}
